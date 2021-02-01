f10013bc248d00a9b098287853d36fbfdcbe72c4
nikothcdmt <76887650+nikothcdmt@users.noreply.github.com>
2021-02-01 16:38:22 -0500

    loaded .txt part

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 
 #include <iostream>
 #include <Windows.h>
+#include <fstream>
+#include <string>
+#include <vector>
 
 
 
-
+// need to add the clipboard part and it's doooneee ha!
 
 int main()
 {
+	std::string line;
+	std::vector<std::string> lines;
+	std::ifstream myFile{"lines.txt"};
+	if (myFile.is_open())
+	{
+		while (myFile >> line)
+		{
+			lines.push_back(line);
+		}
+		myFile.close();
+	}
+	else
+	{
+		std::cout << "could not open lines.txt" << std::endl;
+	}
+
 	while (true)
 	{
 		Sleep(120);
